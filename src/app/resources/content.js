3c7dbc4950fea7b0dda544c4e3c8c40999819df9
ChutiponTri <chutipon.trir@gmail.com>
2025-05-02 19:43:31 +0700

    update recent project

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -59,7 +59,7 @@ const home = {
   featured: {
     display: true,
     title: <>Recent project: <strong className="ml-4">Housepital</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    href: "/work/company-reg",
   },
   subline: (
     <>
